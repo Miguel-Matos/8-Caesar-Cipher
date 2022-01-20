2d0bb9097cbf7b4d89c726c812af75d8ec12fa16
Miguel <protoblae@gmail.com>
2022-01-20 09:26:21 +0900

    QOL improvements

--- 8-Caesar-Cipher.py
+++ 8-Caesar-Cipher.py
@@ -1,8 +1,8 @@
+import art
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+print(art.logo)
 
 def caesar(plain_text, shift_amount, the_direction):
   cipher_text = ""
@@ -14,5 +14,13 @@ def caesar(plain_text, shift_amount, the_direction):
         new_position = position - shift_amount
     cipher_text += alphabet[new_position]
   print(f"The encoded text is {cipher_text}")
-
-caesar(plain_text=text, shift_amount=shift, the_direction=direction)
+  
+rep_loop = False
+while rep_loop == False:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(plain_text=text, shift_amount=shift, the_direction=direction)
+    again = input("Do you want to go again? Yes or No? ").lower()
+    if again == "no":
+        rep_loop = True
